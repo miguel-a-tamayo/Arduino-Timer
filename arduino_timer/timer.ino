0d8b594141ba47990646bad57238a3862fb12fa2
Miguel Tamayo <miatamay@ucsc.edu>
2022-06-07 22:28:34 -0700

    Testing Library

--- arduino_timer/timer.ino
+++ arduino_timer/timer.ino
@@ -3,13 +3,17 @@
 uint8_t segments[] = {A5, 6, 7, 10}; // segment pins
 uint8_t leds[] = {A2, A4, 8, 13, A1, A3, 5, 9}; // led pins
 
-// fourDigit display; // isntance of the 4 digit display class
+fourDigit display; // isntance of the 4 digit display class
 
 void setup() {
     // initiate the display with common cathode and 1s delay
-    // display.begin(0, leds, segments, 1000);
+    display.begin(0, leds, segments, 1000);
 }
 
 void loop() {
-    // display.segmentTurnOn(0, 0);
+    display.segmentTurnOn(0, 0);
+    display.segmentTurnOff(1);
+    display.segmentTurnOff(2);
+    display.segmentTurnOff(3);
+    delay(10);
 }
